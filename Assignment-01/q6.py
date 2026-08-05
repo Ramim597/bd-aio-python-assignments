eb117c4463ba685acc2c54d985e14d816c5522b2
Ramim <191146698+Ramim597@users.noreply.github.com>
2026-08-05 08:13:02 +0600

    add q-06

---
+++ Assignment-01/q6.py
@@ -0,0 +1,10 @@
+# Q6: Write a program to swap values of two numbers entered by the user:
+
+a = int(input("Enter a number:"))
+b = int(input("Enter another number:"))
+
+# tuple unpacking:
+a, b = b, a
+
+print("a =", a)
+print("b =", b)
