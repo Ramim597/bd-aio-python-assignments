9472613802929252f4f44ce2089329357c0c0d57
Ramim <191146698+Ramim597@users.noreply.github.com>
2026-08-05 08:34:35 +0600

    add q8 assg_01

---
+++ Assignment-01/q8.py
@@ -0,0 +1,10 @@
+# Q8: Take the radius (r) as user input and print the area.
+# Use the formula:
+# Area = π × r²
+# (Value of π = 3.14)
+
+r = int(input("Enter circle's Radius:"))
+pi = 3.14
+area = pi * r**2  # formula of circle Area
+
+print("Area =", area)
