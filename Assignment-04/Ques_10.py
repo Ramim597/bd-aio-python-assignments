26a129f6a3a139aadf78829f6c76f2fa9c0c3010
Ramim <191146698+Ramim597@users.noreply.github.com>
2026-08-18 08:59:34 +0600

    solve question 01 assg -05

---
+++ Assignment-04/Ques_10.py
@@ -0,0 +1,13 @@
+# Q10: Mini Project – OOP Chat System
+
+# Let's create a Chat System using OOP concepts.
+# We have to create classes:
+# - User
+# - Message
+# - ChatRoom
+
+# And we have to implement functions:
+# - sending messages
+# - viewing chat history
+# - user joining and leaving the chatroom
+
--- Assignment-05/Ques_01.py
+++ Assignment-05/Ques_01.py
@@ -1,4 +1,13 @@
-# question
+# Q1. Create a program that:
+# 1. Opens a file "names.txt" in write mode
+# 2. Writes 5 names (one per line) entered by the user
+# 3. Then opens the same file in read mode and prints all names
 
-with open("name.txt", "r") as f:
-    print(f.read())
+with open("names.txt", "w") as f:
+    for i in range(1, 6):
+        names = input(f"Enter name {i}: ")
+        f.write(f"{names}\n")
+
+with open("names.txt", "r") as f:
+    content = f.read()
+    print(content)
