2c3102717175dcdc8aca138c991c7beee2e26524
Ramim <191146698+Ramim597@users.noreply.github.com>
2026-08-19 08:56:37 +0600

    solve q-05 assg -05

--- Assignment-05/Ques_05.py
+++ Assignment-05/Ques_05.py
@@ -1,2 +1,8 @@
 # Q5. Write a program that tries to open "data.txt" in read mode.
 # If the file does not exist, catch the exception and print "File not found!".
+
+try:
+    with open("data.txt", "r") as f:
+        print("File found")
+except FileNotFoundError:
+    print("File not found!")
